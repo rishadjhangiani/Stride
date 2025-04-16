745e81c2356c24fa115aebc226dfbf0e66e55558
rishajhangiani <rishajhangiani@outlook.com>
2025-04-16 01:14:18 -0500

    Initial Commit

---
+++ Stride/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stride
+//
+//  Created by Risha Jhangiani on 4/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stride/StrideApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrideApp.swift
+//  Stride
+//
+//  Created by Risha Jhangiani on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct StrideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
