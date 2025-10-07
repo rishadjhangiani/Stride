d50aa491dac0b0f97598596d5cee07810eb7b9a2
rishajhangiani <rishajhangiani@outlook.com>
2025-10-07 15:03:25 -0500

    reinitilized clean stride project

---
+++ Stride/Stride/StrideApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrideApp.swift
+//  Stride
+//
+//  Created by Risha Jhangiani on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct StrideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Stride/StrideTests/StrideTests.swift
@@ -0,0 +1,17 @@
+//
+//  StrideTests.swift
+//  StrideTests
+//
+//  Created by Risha Jhangiani on 10/7/25.
+//
+
+import Testing
+@testable import Stride
+
+struct StrideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
